7c4ccb8541c9d45df8a912a6ec1d3ad33c646a9f
DrSuite <mechon3@gmail.com>
2023-09-01 12:00:41 -0700

    adding file

--- NucampFolder/React/nucampsite/src/App.js
+++ NucampFolder/React/nucampsite/src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import './App.css';
+import NucampLogo from './app/assets/img/logo.png';
+import { Container, Navbar, NavbarBrand } from 'reactstrap';
+
 
 function App() {
   return (
+
     <div className="App">
+      <Navbar dark color='primary' sticky='top' expand='md'>
+        <Container>
+          <NavbarBrand href='/'>
+            <img src={NucampLogo} alt='nucamp logo' />
+          </NavbarBrand>
+        </Container>
+      </Navbar>
+
       Hello Nucamp!
     </div>
   );
