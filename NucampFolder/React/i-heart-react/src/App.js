99fe31409baaff5991838dd22429870253217bf1
DrSuite <mechon3@gmail.com>
2023-09-01 11:33:20 -0700

    Added Heart App

--- NucampFolder/React/i-heart-react/src/App.js
+++ NucampFolder/React/i-heart-react/src/App.js
@@ -1,30 +1,13 @@
-import HeartLogo from './heart.svg';
 import './App.css';
 
-const message = 'cool cud';
-
-const Header= () => {
-  return(
-    <div className='App-header'>
-      <h1>I ❤️ React</h1>
-    </div>
-  )
-}
-
-const Heart = (props) => {
-  return(
-    <div className='heart'>
-      <img className='heart-img' src={HeartLogo} alt='heart' />
-      <p className='heart-message'>{props.msg}</p>
-    </div>
-  );
-}
+import Header from './components/Header';
+import HeartsList from './components/HeartsList';
 
 function App() {
   return (
     <>
     <Header />
-    <Heart msg={message} />
+    <HeartsList />
     </>
   );
 }
---
+++ NucampFolder/React/i-heart-react/src/components/Header.js
@@ -0,0 +1,9 @@
+const Header= () => {
+  return(
+    <div className='App-header'>
+      <h1>I ❤️ React</h1>
+    </div>
+  );
+}
+
+export default Header;
