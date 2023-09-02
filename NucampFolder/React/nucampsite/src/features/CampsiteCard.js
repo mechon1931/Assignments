06de2bdeb8b15e596e1f67f9bbcc64cae778c14b
DrSuite <mechon3@gmail.com>
2023-09-02 12:07:20 -0700

    Adding Header and Footer Files

---
+++ NucampFolder/React/nucampsite/src/features/CampsiteCard.js
@@ -0,0 +1,21 @@
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+
+const CampsiteCard = (props) => {
+
+  return(
+    <Card>
+      <CardImg
+        width='100%'
+        src={props.campsite.image}
+        alt={props.campsite.name}
+      />
+
+      <CardTitle>
+        {props.campsite.name}
+      </CardTitle>
+      
+    </Card>
+  )
+}
+
+export default CampsiteCard;
