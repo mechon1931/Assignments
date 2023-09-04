c59aaf4b6d82ae0e4fd2879bc3a0c1a7c19c4197
DrSuite <mechon3@gmail.com>
2023-09-04 11:46:15 -0700

    added button

--- NucampFolder/React/nucampsite/src/pages/CampsitesDirectoryPage.js
+++ NucampFolder/React/nucampsite/src/pages/CampsitesDirectoryPage.js
@@ -6,13 +6,21 @@ import { selectRandomCampsite } from '../features/campsites/campsitesSlice';
 const CampsiteDirectoryPage = () => {
   const selectedCampsite = selectRandomCampsite();
 
+  const toggleCampsite = () => {
+    selectedCampsite = selectRandomCampsite();
+    console.log(selectedCampsite);
+  };
   return (
     <Container>
+      <Button onClick={() => toggleCampsite()}>
+        Select Random Campsite
+      </Button>
+      
       <Row>
           <Col sm='5' md='7'>
             <CampsitesList />
           </Col>
-          
+
           <Col sm='7' md='5'>
             <CampsiteDetail campsite={selectedCampsite} />
           </Col>
