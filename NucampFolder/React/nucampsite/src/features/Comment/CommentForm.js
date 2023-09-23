c63a3c1232c37d6d52eab5fc207813ea497f1e0b
DrSuite <mechon3@gmail.com>
2023-09-22 17:16:05 -0700

    commentForm updated

---
+++ NucampFolder/React/nucampsite/src/features/Comment/CommentForm.js
@@ -0,0 +1,105 @@
+import { useState } from 'react';
+import { 
+  Button,
+  Modal, 
+  ModalHeader,  
+  FormGroup, 
+  Label } from 'reactstrap';
+import {
+  Formik, 
+  Field, 
+  Form } from 'formik';
+
+
+const CommentForm = ( {campsiteId }) => {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const handleSubmit = (values) => {
+    const comment = {
+      campsiteId: parseInt(campsiteId),
+      rating: values.rating,
+      author: values.author,
+      text: values.commentText
+    };
+    console.log(comment);
+    setModalOpen(false);
+  };
+
+  return(
+    <>
+      <Button 
+        outline 
+        onClick={() => setModalOpen(true)}
+      >
+        <i className='fa fa-pencil fa-lg' /> 
+          Add Comment
+      </Button>
+
+      <Modal 
+        isOpen={modalOpen} 
+        toggle={() => setModalOpen(false)}
+      >
+      <ModalHeader toggle={() => setModalOpen(false)}>
+        Add Comment
+      </ModalHeader>
+      
+        <Formik
+          initialValues={{
+            rating: undefined,
+            author: '',
+            commentText: '',
+          }}
+          onSubmit={handleSubmit}
+        >
+          <Form>
+            <FormGroup>
+              <Label htmlFor='rating'>
+                Rating
+              </Label>
+              <Field
+                name='rating'
+                as='select'
+                className='form-control'
+              >
+                <option>Select...</option>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+              </Field>
+            </FormGroup>
+
+            <FormGroup>
+              <Label htmlFor='author'>
+                Your Name
+              </Label>
+              <Field
+                name='author'
+                placeholder='Your Name Here'
+                className='form-control'
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <Label htmlFor='commentText'>
+                Comment
+              </Label>
+              <Field
+                name='commentText'
+                as='textarea'
+                rows='12'
+                className='form-control'
+              />
+            </FormGroup>
+            <Button type='submit' color='primary'>
+              Submit
+            </Button>
+          </Form>
+        </Formik>
+      </Modal>
+    </>
+  )
+}
+
+export default CommentForm;
