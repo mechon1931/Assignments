36d7e63aebf9b40eb1a28fc94e35a76bd12ffcd8
DrSuite <mechon3@gmail.com>
2023-09-28 19:12:07 -0700

    Header and UserloginForm added

--- NucampFolder/React/nucampsite/src/components/Header.js
+++ NucampFolder/React/nucampsite/src/components/Header.js
@@ -9,6 +9,7 @@ import {
 } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 import NucampLogo from '../app/assets/img/logo.png';
+import UserLoginForm from '../features/user/UserLoginForm';
 
 const Header = () => {
 
@@ -49,6 +50,8 @@ const Header = () => {
             </NavLink>
           </NavItem>
         </Nav>
+
+        <UserLoginForm />
       </Collapse>
     </Navbar>
   );
