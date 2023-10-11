0db342cce3f71eacf2b058178d5482442bde3045
DrSuite <mechon3@gmail.com>
2023-10-10 19:22:17 -0700

    directoryScreen and Main Component added to app

--- NucampFolder/ReactNative.nosync/nucampsite/App.js
+++ NucampFolder/ReactNative.nosync/nucampsite/App.js
@@ -1,20 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Main from './screens/MainComponent';
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>My first app</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Main />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
