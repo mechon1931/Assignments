2924c4d607c221866250d625205bf46db4ecdfc3
DrSuite <mechon3@gmail.com>
2023-10-13 10:06:47 -0700

    updated screen

--- NucampFolder/ReactNative.nosync/nucampsite/screens/MainComponent.js
+++ NucampFolder/ReactNative.nosync/nucampsite/screens/MainComponent.js
@@ -1,20 +1,49 @@
-import { useState } from 'react';
-import { View } from 'react-native';
-import { CAMPSITES } from '../shared/campsites';
+import { Platform, View } from 'react-native';
 import CampsiteInfoScreen from './CampsiteInfoScreen';
 import DirectoryScreen from './DirectoryScreen';
+import Constants from 'expo-constants';
+import { createStackNavigator } from '@react-navigation/stack';
 
+const DirectoryNavigatior = () => {
+    const Stack = createStackNavigator();
+
+    return(
+        <DirectoryNavigatior>
+            <Stack.Navigator
+                initialRouteName='Directory'
+                screenOptions={{
+                    headerStyle: {
+                        backgroundColor: '#5637DD'
+                    },
+                    headerTintColor: '#fff'
+                }}
+            >
+                <Stack.Screen 
+                    name='Directory'
+                    component={DirectoryScreen}
+                    options={{title: 'Campsite Directory'}}
+                />
+                <Stack.Screen 
+                    name='CampsiteInfo'
+                    component={CampsiteInfoScreen}
+                    options={ ({ route }) => ({
+                        title: route.params.campsite.name
+                    })
+                    }
+                />
+            </Stack.Navigator>
+        </DirectoryNavigatior>
+    )
+}
 const Main = () => {
-    const [campsites, setCampsites] = useState(CAMPSITES);
-    const [selectedCampsiteId, setSelectedCampsiteId] = useState();
 
     return (
-        <View style={{ flex: 1 }}>
-            <DirectoryScreen
+        <View style={{ flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight }}>
+            <DirectoryNavigatior
                 campsites={campsites}
                 onPress={(campsiteId) => setSelectedCampsiteId(campsiteId)}
             />
-            <CampsiteInfoScreen
+            <DirectoryNavigatior
                 campsite={
                     campsites.filter(
                         (campsite) => campsite.id === selectedCampsiteId
