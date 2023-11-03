7831cee7df2dcd842a0094a749130c9215acb340
mechon1931 <mechon3@gmail.com>
2023-11-03 14:27:52 -0700

    Notifications added

--- NucampFolder/ReactNative.nosync/nucampsite/screens/ReservationScreen.js
+++ NucampFolder/ReactNative.nosync/nucampsite/screens/ReservationScreen.js
@@ -11,6 +11,7 @@ import {
 import { Picker } from '@react-native-picker/picker';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as Animatable from 'react-native-animatable';
+import * as Notifications from 'expo-notifications';
 
 
 const ReservationScreen = () => {
@@ -18,7 +19,6 @@ const ReservationScreen = () => {
     const [hikeIn, setHikeIn] = useState(false);
     const [date, setDate] = useState(new Date());
     const [showCalendar, setShowCalendar] = useState(false);
-    const [showModal, setShowModal] = useState(false);
 
     const onDateChange = (event, selectedDate) => {
         const currentDate = selectedDate || date;
@@ -27,28 +27,37 @@ const ReservationScreen = () => {
     };
 
     const handleReservation = () => {
+        const message = `Number of Campers: ${campers}
+                            \nHike-In? ${hikeIn}
+                            \nDate: ${date.toLocaleDateString('en-US')}`;
 
         Alert.alert(
-            'Confirm Reservation',
-            `Number of Campers: ${campers}
-                \nHike-In: ${hikeIn ? 'true' : 'false'}
-                \nDate: ${date.toLocaleDateString('en-US')}`,
+            'Begin Search?',
+            message,
             [
                 {
                     text: 'Cancel',
-                    style: 'cancel',
                     onPress: () => {
+                        console.log('Reservation Search Canceled');
                         resetForm();
                     },
+                    style: 'cancel',
                 },
                 {
                     text: 'OK',
-                    /*onPress: () => {
-
-                    }, */
+                    onPress: () => {
+                        presentLocalNotification(
+                            date.toLocaleDateString('en-US')
+                        );
+                        resetForm();
+                    }
                 },
-            ]
+            ],
+            { cancelable: false }
         );
+        console.log('campers:', campers);
+        console.log('hikeIn:', hikeIn);
+        console.log('date', date);
     };
 
     const resetForm = () => {
@@ -58,15 +67,41 @@ const ReservationScreen = () => {
         setShowCalendar(false);
     };
 
+    const presentLocalNotification = async(reservationDate) => {
+        const sendNotification = () => {
+            Notifications.setNotificationHandler({
+                handleNotification: async() => ({
+                    shouldShowAlert: true,
+                    shouldPlaySound: true,
+                    shouldSetBadge: true
+                })
+            });
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: 'Your Campsite Reservation Search',
+                    body: `Search for ${reservationDate} requested`
+                },
+                trigger: null
+            });
+        }
+        let permissions = await Notifications.getPermissionsAsync();
+        
+        if(!permissions.granted) {
+            permissions = await Notifications.requestPermissionsAsync();
+        };
+
+        if(permissions.granted) {
+            sendNotification();
+        }
+    };
+
     return (
         <ScrollView>
             <Animatable.View
                 animation="zoomIn"
-                duration={2}
-                delay={1}
+                duration={2000}
+                delay={1000}
             >
-                
-            
                 <View style={styles.formRow}>
                     <Text style={styles.formLabel}>Number of Campers:</Text>
                     <Picker
