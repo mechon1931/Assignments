f04838e476f83f8f9c7d7d414509b8282f5ce7b5
Mechon1931 <mechon3@gmail.com>
2023-11-17 13:26:58 -0800

    Express Router

--- NucampFolder/NodeJS-Express/node-express/server.js
+++ NucampFolder/NodeJS-Express/node-express/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan'); 
+const campsiteRouter = require('./routes/campsiteRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -8,28 +9,7 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
 
-app.all('/campsites', (req, res, next) => {
-    res.status = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-});
-
-app.get('/campsites', (req, res) => {
-    res.end('Will send all the campsites to you');
-});
-
-app.post('/campsites', (req, res) => {
-    res.end(`Will add the campsites: ${req.body.name} with description: ${req.body.description}`);
-});
-
-app.put('/campsites', (req, res) => {
-    res.statusCode = 403;
-    res.end('PUT operation not supported on /campsites');
-});
-
-app.delete('/campsites/:campsiteId', (req, res) => {
-    res.end('Deleting campsite: ${req.params.campsiteId}');
-});
+app.use('/campsites', campsiteRouter);
 
 app.use(express.static(__dirname + '/public'));
 
