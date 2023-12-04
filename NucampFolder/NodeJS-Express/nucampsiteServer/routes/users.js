4538703b4da572121eb536fda1c00b6e09824e60
Mechon1931 <mechon3@gmail.com>
2023-12-03 17:34:40 -0800

    populate

--- NucampFolder/NodeJS-Express/nucampsiteServer/routes/users.js
+++ NucampFolder/NodeJS-Express/nucampsiteServer/routes/users.js
@@ -14,20 +14,34 @@ router.post('/signup', (req, res) => {
   User.register(
     new User( { username: req.body.username }),
     req.body.password,
-    err => {
+    (err, user) => {
       if(err) {
-        res.statusCode = 500;
+        res.statusCode = 500,
         res.setHeader('Content-Type', 'application/json');
         res.json({err: err});
       } else {
-        passport.authenticate('local')(req, res, () => {
-          res.statusCode = 200;
-          res.setHeader('Content-type', 'application/json');
-          res.json({success: true, status: 'Registration Successful!'});
-        });
+        if(req.body.firstname) {
+          user.firstname = req.body.firstname;
+        }
+        if(req.body.lastname) {
+          user.lastname = req.body.lastname;
+        }
+        user.save(err => {
+          if(err) {
+          res.statusCode = 500;
+          res.setHeader('Content-Type', 'application/json');
+          res.json({err: err});
+          return;
+          } 
+          passport.authenticate('local')(req, res, () => {
+            res.statusCode = 200;
+            res.setHeader('Content-type', 'application/json');
+            res.json({success: true, status: 'Registration Successful!'});
+          });
+        })
       }
     }
-  )
+  );
 });
 
 router.post('/login', passport.authenticate('local'), (req, res) => {
@@ -35,35 +49,6 @@ router.post('/login', passport.authenticate('local'), (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-type', 'application/json');
   res.json({success: true, token: token, status: 'You are successfully logged in!'});
-});
-
-    const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-    const username = auth[0];
-    const password = auth[1];
-    
-    User.findOne({ username: username })
-    .then(user => {
-      if(!user) {
-        const err = new Error(`User ${username} does not exists!`);
-        err.statusCode = 401;
-        return next(err);
-      } else if(user.password !== password) {
-        const err = new Error('Your password is incorrect!');
-        err.statusCode = 401;
-        return next(err);
-      } else if(user.username === username && user.password === password) {
-        req.session.user = 'authenticated';
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('You are authenticated!');
-      }
-    })
-    .catch(err => next(err));
-  } else {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('You are already authenticated!');
-  }
 });
 
 router.get('/logout', (req, res, next) => {
