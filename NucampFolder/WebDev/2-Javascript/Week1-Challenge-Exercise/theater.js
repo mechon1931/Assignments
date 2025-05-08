49d2e6701abdc066caa1fcda9d912b9d86f59a66
mechon1931 <mechon3@gmail.com>
2025-05-07 21:17:38 -0700

    input button validations

--- NucampFolder/WebDev/2-Javascript/Week1-Challenge-Exercise/theater.js
+++ NucampFolder/WebDev/2-Javascript/Week1-Challenge-Exercise/theater.js
@@ -7,19 +7,31 @@ function buyTicket() {
     const age = prompt('What is your age?');
     let cost = getBaseTicketCost(age);
     const isMatinee = prompt("Are you attending a matinee show?");
+    cost = getMatineeDiscount(cost, isMatinee);
 
-    if((isMatinee === "yes" || isMatinee === "y")) {
-        cost = cost - MATINEE_DISCOUNT;
-    }
-
-    
-    alert("Your ticket will cost: $" +cost)
+    alert("Your ticket will cost: $" + cost)
 
 }
 
 function getBaseTicketCost(age) {
-    if ((age <= 12) || (age >= 65)) {
+    if (age < 0 ) {
+        return "You cannot have negative age numbers!!! Please ENTER A POSITIVE NUMBER";
+    } else if (typeof age != "number" || isNaN(age)) {
+        return 'Please enter a valid number';
+    } else if ((age <= 12) || (age >= 65)) {
         return CHILD_AND_SENIOR_TICKET_COST
+    } else {
+        return GENERAL_ADMISSION_TICKET_COST;
     }
-    return GENERAL_ADMISSION_TICKET_COST;
+    
+}
+
+function getMatineeDiscount(cost, isMatinee) {
+    isMatinee = isMatinee.toLowerCase();
+
+    if((isMatinee === "yes" || isMatinee === "y")) {
+        return cost = cost - MATINEE_DISCOUNT;
+    }
+
+    return cost
 }
