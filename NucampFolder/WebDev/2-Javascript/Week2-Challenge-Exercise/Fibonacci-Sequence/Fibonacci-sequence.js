7096608d9a22c98a5ad0b85806a8406f99ae8518
mechon1931 <mechon3@gmail.com>
2025-05-14 17:40:07 -0700

    added for loop

--- NucampFolder/WebDev/2-Javascript/Week2-Challenge-Exercise/Fibonacci-Sequence/Fibonacci-sequence.js
+++ NucampFolder/WebDev/2-Javascript/Week2-Challenge-Exercise/Fibonacci-Sequence/Fibonacci-sequence.js
@@ -1,4 +1,4 @@
-
+/*
 function printFibonacci() {
 let a = 0;
 let b = 1;
@@ -15,3 +15,20 @@ while(count < 10) {
 }
 
 printFibonacci(10)
+*/
+
+function printFibonacci() {
+    let a = 0;
+    let b = 1;
+    let count = 0;
+
+    for (let i = 0; i < 10; i++) {
+        console.log(a)
+        let temp = a + b;
+        a = b;
+        b = temp;
+        count++;
+    }
+}
+
+printFibonacci(5)
